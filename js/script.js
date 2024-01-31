688745793e6f554ecf743022483c5a8afbf3a2de
Axhu99 <xhuliodreni@icloud.com>
2024-01-31 09:32:58 +0100

    fix bonus

--- js/script.js
+++ js/script.js
@@ -3,23 +3,23 @@ const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail'
 const app = Vue.createApp({
     data: () => ({
         emails: [],
-        isLoading: false,
-
+        tot: 10,
     }),
-
-    created(){
-        for(let i = 0; i < 10; i++){
+    
+    methods:{
+        // recupero UNA sola email 
+        fetchData(){
             axios.get(endpoint).then(res =>{
                 this.emails.push(res.data.response)
-            }).catch(err =>{    
-                console.log(err)
-            }).then(() =>{
-                    this.isLoading = false;
             })
         }
-
+    },
+    created(){
+        // alla creazione della pagina recupero un email TOT volte
+        for(i = 0; i < this.tot; i++){
+            this.fetchData();
+        } 
     }
-    
 });
 
-app.mount('#root')
+app.mount('#root')   
